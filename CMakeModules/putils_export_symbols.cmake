c656a09b400c5c55db4e45632f66762f7ae55399
Nicolas Phister <nicolas.phister@dont-nod.com>
2022-11-16 11:32:05 +0100

    putils_export_symbols: force include export header

--- CMakeModules/putils_export_symbols.cmake
+++ CMakeModules/putils_export_symbols.cmake
@@ -1,7 +1,32 @@
 function(putils_export_symbols target)
     include(GenerateExportHeader)
     generate_export_header(${target} ${ARGN})
+
+    # Get the same EXPORT_FILE_NAME as generate_export_header (see GenerateExportHeader.cmake)
+    set(options)
+    set(oneValueArgs EXPORT_FILE_NAME)
+    set(multiValueArgs)
+    cmake_parse_arguments(_GEH "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+    set(BASE_NAME ${target})
+    string(TOLOWER ${BASE_NAME} BASE_NAME_LOWER)
+    set(EXPORT_FILE_NAME "${CMAKE_CURRENT_BINARY_DIR}/${BASE_NAME_LOWER}_export.h")
+    if(_GEH_EXPORT_FILE_NAME)
+        if(IS_ABSOLUTE ${_GEH_EXPORT_FILE_NAME})
+            set(EXPORT_FILE_NAME ${_GEH_EXPORT_FILE_NAME})
+        else()
+            set(EXPORT_FILE_NAME "${CMAKE_CURRENT_BINARY_DIR}/${_GEH_EXPORT_FILE_NAME}")
+        endif()
+    endif()
+
+    # The header will be generated here, so add it to the include directories
     target_include_directories(${target} PUBLIC ${CMAKE_CURRENT_BINARY_DIR})
+
+    # Force include the header
+    if (MSVC)
+        target_compile_options(${target} PUBLIC /FI${EXPORT_FILE_NAME})
+    else()
+        target_compile_options(${target} PUBLIC "SHELL:-include ${EXPORT_FILE_NAME}")
+    endif()
 
     if (MSVC)
         # Disable warning "'...' needs to have dll-interface to be used by clients of class '...'"
