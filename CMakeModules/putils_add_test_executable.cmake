7c5b1a9e30aec501a8e6cab94bcb3f807614e49d
Nicolas Phister <nicolas.phister@dont-nod.com>
2022-11-16 16:01:52 +0100

    fix tests not detected with BUILD_SHARED_LIBS=ON

--- CMakeModules/putils_add_test_executable.cmake
+++ CMakeModules/putils_add_test_executable.cmake
@@ -4,13 +4,12 @@ function(putils_add_test_executable test_exe_name)
 
 	set(customFindPackageNames gtest:GTest)
 	set(customLibraryNames gtest:GTest)
-	putils_conan_set_shared_options(options gtest)
+	# Setting gtest:shared=True causes the tests to not be found for some reason
 	putils_conan_download_and_link_packages_with_names(
 		${test_exe_name} PRIVATE
 		"${customFindPackageNames}"
 		"${customLibraryNames}"
-		gtest/cci.20210126
-		${options}
+		gtest/1.12.1
 	)
 
 	include(GoogleTest)
