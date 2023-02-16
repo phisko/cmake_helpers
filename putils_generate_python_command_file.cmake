cf7d771655ea8d0ac467905a9c996d4a807347c4
Nicolas Phister <nicolas.phister@dont-nod.com>
2023-02-16 11:46:24 +0100

    putils_generate_python_command_file: split command into array
    as expected by subprocess.run

--- putils_generate_python_command_file.cmake
+++ putils_generate_python_command_file.cmake
@@ -1,12 +1,12 @@
 # Circumvent cmd's 8191 character limit by generating a Python file that will run the command
 # An alternative would be to use response files, but I haven't found a way to tell CMake or Ninja to do so
 function(putils_generate_python_command_file command_file command)
-    string(JOIN " " command_string ${command})
+    string(JOIN "', '" command_array ${command})
     set(
             file_content
             "
 import subprocess
-subprocess.run('${command_string}')
+subprocess.run(['${command_array}'])
 "
     )
 
