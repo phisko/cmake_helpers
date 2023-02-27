8c831e87f323f1bfbca24347764a1c605df4508d
Nicolas Phister <nicolas.phister@dont-nod.com>
2023-02-27 14:13:11 +0100

    fix find_package(GTest)

--- putils_add_test_executable.cmake
+++ putils_add_test_executable.cmake
@@ -2,10 +2,8 @@ function(putils_add_test_executable test_exe_name)
 	add_executable(${test_exe_name} ${ARGN})
 	putils_copy_dlls(${test_exe_name})
 
-	if(NOT TARGET gtest::gtest)
-		find_package(GTest 1.12.1 REQUIRED)
-	endif()
-	target_link_libraries(${test_exe_name} PRIVATE gtest::gtest)
+	find_package(GTest CONFIG REQUIRED)
+	target_link_libraries(${test_exe_name} PRIVATE GTest::gtest GTest::gtest_main)
 
 	include(GoogleTest)
 	gtest_discover_tests(${test_exe_name} DISCOVERY_MODE PRE_TEST)
